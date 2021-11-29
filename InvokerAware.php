501f78217b944c8cfdfa5f4e6dd2e2a07f9573d4
Joshua Parker <josh@joshuaparker.blog>
2021-11-28 18:12:16 -0800

    Added new trait.

---
+++ InvokerAware.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Qubus\Inheritance
+ *
+ * @link       https://github.com/QubusPHP/inheritance
+ * @copyright  2021 Joshua Parker <josh@joshuaparker.blog>
+ * @license    https://opensource.org/licenses/mit-license.php MIT License
+ *
+ * @since      2.0.0
+ */
+
+declare(strict_types=1);
+
+namespace Qubus\Inheritance;
+
+trait InvokerAware
+{
+    /**
+     * Call the given Closure with buffering support.
+     *
+     * @param callable|Closure  $closure
+     * @param array     $parameters
+     * @param bool      $buffer
+     * @return mixed
+     */
+    public function call($closure, array $parameters = [], bool $buffer = false)
+    {
+        if ($buffer) {
+            ob_start();
+        }
+
+        $result = call_user_func_array($closure, $parameters);
+
+        if ($buffer) {
+            return ob_get_clean();
+        }
+
+        return $result;
+    }
+}
